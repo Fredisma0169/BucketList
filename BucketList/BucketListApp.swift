78cd51c0d7be5ce3076d49dcc2d7ad29f0176a4d
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-03-08 20:47:24 +0100

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Fredisma OUEDRAOGO on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
